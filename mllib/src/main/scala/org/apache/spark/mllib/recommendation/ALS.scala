e7873ff008df56079e74f3170ce8122d434c996c
witgo <witgo@qq.com>
2014-05-31 15:53:12 +0800

    Improve ALS algorithm resource usage

--- mllib/src/main/scala/org/apache/spark/mllib/recommendation/ALS.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/recommendation/ALS.scala
@@ -211,6 +211,11 @@ class ALS private (
       for (iter <- 1 to iterations) {
         // perform ALS update
         logInfo("Re-computing I given U (Iteration %d/%d)".format(iter, iterations))
+        // All references to users parent RDDs will be removed,
+        // so that be cleaned in sc.cleaner
+        if (sc.checkpointDir.isDefined && (iter % 3 == 1)) {
+          users.checkpoint()
+        }
         // Persist users because it will be called twice.
         users.persist()
         val YtY = Some(sc.broadcast(computeYtY(users)))
@@ -219,6 +224,9 @@ class ALS private (
           alpha, YtY)
         previousProducts.unpersist()
         logInfo("Re-computing U given I (Iteration %d/%d)".format(iter, iterations))
+        if (sc.checkpointDir.isDefined && (iter % 3 == 1)) {
+          products.checkpoint()
+        }
         products.persist()
         val XtX = Some(sc.broadcast(computeYtY(products)))
         val previousUsers = users
